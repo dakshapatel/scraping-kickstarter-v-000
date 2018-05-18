c35ad78fd039ad224dac24823d45c8b55f3268c4
Daksha Patel <dakshapatel100@gmail.com>
2018-05-18 15:46:38 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,12 +19,12 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
-
+  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
 end
 
-projects 
+projects
 end
 
 project.css("p.bbcard_blurb").text
