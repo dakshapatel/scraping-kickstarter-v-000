e946cac973f07160973ded6d8a076d7d4494ba0f
Daksha Patel <dakshapatel100@gmail.com>
2018-05-18 15:10:32 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,12 @@
 # require libraries/modules here
+# projects: kickstarter.css("li.project.grid_4")
+
+#The variable_name = _ syntax used in Pry will assign the variable name to the return value of whatever was executed above.
+
+
 require 'nokogiri'
 require 'pry'
+class Kickstarter
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -8,3 +14,5 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
+
+end 
