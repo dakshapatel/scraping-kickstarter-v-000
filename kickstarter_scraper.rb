5d930055d45093a1d4535ee6b55783082ba4fbf6
Daksha Patel <dakshapatel100@gmail.com>
2018-05-18 15:55:27 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -27,9 +27,9 @@ def create_project_hash
     #symbols make better hash keys than strings.
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("span.location-name").text,
-      :percent_funded => project.css("li.first.funded strong").text.gsub("%","").to_i
+       :description => project.css("p.bbcard_blurb").text,
+       :location => project.css("ul.project-meta span.location-name").text,
+       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
 end
 #return the projects hash
