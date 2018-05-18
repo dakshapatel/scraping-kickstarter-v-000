ccfceb96702c9c26481ecce50205c91521955003
Daksha Patel <dakshapatel100@gmail.com>
2018-05-18 15:55:04 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -28,13 +28,13 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :location => project.css("span.location-name").text,
+      :percent_funded => project.css("li.first.funded strong").text.gsub("%","").to_i
     }
 end
 #return the projects hash
 projects
 end
-
+end 
 #project.css("p.bbcard_blurb").text
 #Kickstarter.new.create_project_hash
