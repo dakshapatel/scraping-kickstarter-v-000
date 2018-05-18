67295f64fa35ef120c6d7a0b76e1572119d14437
Daksha Patel <dakshapatel100@gmail.com>
2018-05-18 15:35:04 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
 
 #The variable_name = _ syntax used in Pry will assign the variable name to the return value of whatever was executed above.
 
@@ -21,5 +22,5 @@ end
 
 end
 
-
+project.css("p.bbcard_blurb").text
 Kickstarter.new.create_project_hash
